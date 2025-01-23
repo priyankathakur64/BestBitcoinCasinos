2a8b1c4a9922811baa71964872a07e46d30987c0
priyankathakur64 <priyanka.6432thakur@gmail.com>
2025-01-23 12:16:22 +0530

    Updated API URL with environment variable

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,11 +3,15 @@ import axios from "axios";
 // Function to fetch the report data
 const fetchReportData = async (fromDate, toDate, casino) => {
   try {
-    const response = await axios.post("/api/customer/v1/partner/report", {
-      fromDate,
-      toDate,
-      casino,
-    });
+    const baseUrl = process.env.REACT_APP_API_URL || "";
+    const response = await axios.post(
+      `${baseUrl}/api/customer/v1/partner/report`,
+      {
+        fromDate,
+        toDate,
+        casino,
+      }
+    );
     if (response.data && response.data.data) {
       return response.data.data; // Return the filtered data
     } else {
